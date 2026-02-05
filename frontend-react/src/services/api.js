bc1af4727f976c21e1dfa3e86ba2ea5666fbd066
Ish <ish@epochly.com>
2026-02-04 19:21:08 -0600

    Fix: Add ngrok-skip-browser-warning header

--- frontend-react/src/services/api.js
+++ frontend-react/src/services/api.js
@@ -7,6 +7,7 @@ const api = axios.create({
     timeout: 10000, // 10 seconds timeout
     headers: {
         'Content-Type': 'application/json',
+        'ngrok-skip-browser-warning': 'true',
     },
 });
 
